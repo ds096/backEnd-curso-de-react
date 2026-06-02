ad678fdd9c5b18864fac99961cc54d95ff70e5f3
ds096 <ss.daniel096@gmail.com>
2026-06-02 08:44:52 -0300

    Finish functions CRUDE

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
 import express from "express";
-import prisma from "./lib/prisma.ts";
+import { PrismaClient } from "./generated/prisma/index.js";
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-const tasks = [];
-
-app.post("/", (req, res) => {
-  tasks.push(req.body);
+app.post("/", async (req, res) => {
+  await prisma.task.create({
+    data: {
+      title: req.body.title,
+      description: req.body.description,
+    },
+  });
   res.status(201).json(req.body);
 });
 
-app.get("/", (req, res) => {
+app.put("/:id", async (req, res) => {
+  console.log(req);
+  await prisma.task.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      title: req.body.title,
+      description: req.body.description,
+      isCompleted: req.body.isCompleted,
+    },
+  });
+
+  res.status(200).json(req.body);
+});
+
+app.get("/", async (req, res) => {
+  const tasks = await prisma.task.findMany();
   res.status(200).json(tasks);
+});
+
+app.delete("/:id", async (req, res) => {
+  await prisma.task.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json({ message: "Task deleted successfully" });
 });
 
 app.listen(3000);
